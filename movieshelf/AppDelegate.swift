11261f8f24d990c830d788b7ffbd2b6e917b4ff0
leandrogaluzzi <leandrogaluzzi@gmail.com>
2020-07-12 00:31:55 +0200

    Removing unneded code

--- movieshelf/AppDelegate.swift
+++ movieshelf/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  movieshelf
-//
-//  Created by leandrogaluzzi on 12/07/2020.
-//  Copyright © 2020 leandrogaluzzi. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
@@ -19,21 +11,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         return true
     }
-
-//    // MARK: UISceneSession Lifecycle
-//
-//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-//        // Called when a new scene session is being created.
-//        // Use this method to select a configuration to create the new scene with.
-//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-//    }
-//
-//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-//        // Called when the user discards a scene session.
-//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-//    }
-
-
 }
 
